69bc7d7173aa6c1e4ea0da2fee74b729475bfaba
log27250 <log27250@gmail.com>
2026-09-12 01:41:28 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = 'hakim-optic-app-v1';
+const APP_SHELL = [
+  '/Hakim-Optic-/',
+  '/Hakim-Optic-/index.html',
+  '/Hakim-Optic-/manifest.json',
+  '/Hakim-Optic-/icons/icon-192.png',
+  '/Hakim-Optic-/icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(APP_SHELL))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+    )).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  const req = event.request;
+  if (req.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(req).then(cached => {
+      if (cached) return cached;
+
+      return fetch(req).then(response => {
+        if (response && (response.ok || response.type === 'opaque')) {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(req, copy)).catch(()=>{});
+        }
+        return response;
+      }).catch(() => {
+        if (req.mode === 'navigate') return caches.match('/Hakim-Optic-/index.html');
+        return new Response('', {status: 503, statusText: 'Offline'});
+      });
+    })
+  );
+});
